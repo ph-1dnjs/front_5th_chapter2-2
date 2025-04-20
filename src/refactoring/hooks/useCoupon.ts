be7cde704a7b78d27a8a1b2ade21dabe158f8aec
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-20 18:47:02 +0900

    feat: useCoupons 훅 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,11 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  const handleCouponAdd = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+
+  return { coupons: coupons, addCoupon: handleCouponAdd };
 };
