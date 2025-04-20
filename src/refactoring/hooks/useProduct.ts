bd62d03d1f0f8ca0b30b7f369c57f857fa201470
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-20 18:45:50 +0900

    feat: useProducts 훅 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const handleProductUpdate = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p))
+    );
+  };
+
+  const handleProductAdd = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products: products,
+    updateProduct: handleProductUpdate,
+    addProduct: handleProductAdd,
   };
 };
