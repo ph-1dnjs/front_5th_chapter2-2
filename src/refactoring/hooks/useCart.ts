3c1fb670a806d122ab7d30df66b9ef6b8099d7ad
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-23 21:49:35 +0900

    feat: 장바구니 localStorage 적용

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -2,9 +2,11 @@
 import { useState } from "react";
 import { CartItem, Coupon, Product } from "../../types";
 import { calculateCartTotal, updateCartItemQuantity } from "../models/cart";
+import useLocalStorage from "./useLocalStorage";
 
 export const useCart = () => {
-  const [cart, setCart] = useState<CartItem[]>([]);
+  const [cart, setCart] = useLocalStorage<CartItem[]>("cart", []);
+
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
   const addToCart = (product: Product) => {
