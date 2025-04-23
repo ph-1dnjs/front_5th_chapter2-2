fd520934fdd22b35784cfd9b3ed0b13cb6cd571f
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-23 19:27:13 +0900

    refactor: AdminProductList 컴포넌트 분리

--- src/refactoring/components/product/index.ts
+++ src/refactoring/components/product/index.ts
@@ -1,3 +1,4 @@
 export { default as ProductCard } from "./ProductCard";
 export { default as ProductList } from "./ProductList";
 export { default as NewProductForm } from "./NewProductForm";
+export { default as AdminProductList } from "./AdminProductList";
